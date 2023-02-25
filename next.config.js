2b2c4b342f19053f240e3535a3aa4807e9c12b5d
kdensa <83403467+kdensa-bgbg@users.noreply.github.com>
2023-02-25 04:21:10 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = withPWA({
     images: {
-        domains: [ "cdn.discordapp.com" ]
+        domains: [ "cdn.discordapp.com" ]https://github.com/kdensa-bgbg/awardbot.me/blob/main/next.config.js
     },
     i18n: {
         locales: Object.keys(require("./locales.config.js")),
